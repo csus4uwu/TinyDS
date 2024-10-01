bdb7276f8a3dbcac6aa4969782522d3ffaa0dfc0
Csus4 <2334085989@qq.com>
2024-10-02 00:59:34 +0800

    <update> ArrayTest test1 + test2

--- Sequence/ArrayTest.cpp
+++ Sequence/ArrayTest.cpp
@@ -3,9 +3,28 @@
 
 using std::cin, std::cout, std::cerr, std::endl;
 
+struct Info {
+    short A;
+    double B;
+
+    Info(const short &_A = 0, const double &_B = 0) : A(_A), B(_B) {}
+
+    void display() {
+        cout << this->A << " " << this->B << endl;
+    }
+};
+
+Info operator+(const Info &a, const Info &b) {
+    Info c;
+    c.A = a.A * b.A;
+    c.B = (a.B - b.B) / 2;
+    return c;
+}
+
 int main() {
     cin.tie(nullptr)->sync_with_stdio(false);
 
+    // test1
     Array<int, 5> a;
 
     a.fill(123);
@@ -14,7 +33,6 @@ int main() {
     a[2] = 1;
     a[3] = 2;
     a[4] = 1e9;
-    cout << a[4] << endl;
 
     cout << a.front() << " " << a.back() << endl;
 
@@ -22,6 +40,18 @@ int main() {
         cout << *it << " ";
     }
     endl(cout);
+
+    // test2
+    Array<Info, 3> b;
+    b.front() = {1, -10};
+    b[2].A = 50;
+    b[2].B = 0.5;
+    b[1] = b.front() + b.back();
+    
+    b[1].display();
+    for (auto it = b.begin(); it != b.end(); it++) {
+        it->display();
+    }
 
     return 0;
 }
