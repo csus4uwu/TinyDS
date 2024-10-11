36f714cdb80444ed3deb6a895d8ea45d0d3296aa
Csus4 <2334085989@qq.com>
2024-10-11 17:19:07 +0800

    <update> Stack (todo)

---
+++ Sequence/Stack.hpp
@@ -0,0 +1,18 @@
+// TODO
+#ifndef Stack_H
+#define Stack_H 1
+
+#include <cstddef>   // size_t
+#include <stdexcept> // std::out_of_range
+
+template <typename T>
+struct Stack {
+    T elements[N];
+    int top_;
+
+    T const top() {
+
+    }
+};
+
+#endif
