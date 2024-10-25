3e8dc559baa0b745dc126d1b621dbc8bb5f1af25
Csus4 <2334085989@qq.com>
2024-10-25 23:50:20 +0800

    <update

--- Sequence/CArray.hpp
+++ Sequence/CArray.hpp
@@ -1,73 +1,61 @@
-// TODO
-#ifndef Array_H
-#define Array_H 1
+#ifndef CArray_H
+#define CArray_H 1
 
 #include <cstddef>   // size_t
 #include <stdexcept> // std::out_of_range
 
-template <typename T, size_t N>
-struct Array {
-    T elements[N];
+constexpr int MAX_SIZE = 1e7;
 
-    T &operator[](size_t _pos) noexcept {
-        if (_pos >= N) {
+template <typename T>
+struct CArray {
+    CArray() {
+        elements = new T[MAX_SIZE];
+    }
+    ~CArray() {
+        delete[] elements;
+        elements = nullptr;
+    }
+    T const &operator[](size_t _pos) const {
+        if (_pos >= MAX_SIZE) {
             throw std::out_of_range("invalid position");
         }
         return elements[_pos];
     }
-
-    T const &operator[](size_t _pos) const noexcept {
-        if (_pos >= N) {
+    T &operator[](size_t _pos) {
+        if (_pos >= MAX_SIZE) {
             throw std::out_of_range("invalid position");
         }
         return elements[_pos];
     }
-
-    void fill(T const &_val) noexcept {
-        for (size_t i = 0; i < N; i++) {
+    T *data() {
+        return elements;
+    }
+    void fill(size_t _l, size_t _r, T const &_val) {
+        for (size_t i = _l; i < _r; i++) {
             elements[i] = _val;
         }
     }
-
+    void fill(T const &_val) {
+        fill(0, MAX_SIZE, _val);
+    }
+    void clear(size_t _l = 0, size_t _r = MAX_SIZE) {
+        fill(_l, _r, {});
+    }
     T &front() noexcept {
         return elements[0];
     }
-
     T const &front() const noexcept {
         return elements[0];
     }
-
     T &back() noexcept {
-        return elements[N - 1];
+        return elements[MAX_SIZE - 1];
     }
-
     T const &back() const noexcept {
-        return elements[N - 1];
+        return elements[MAX_SIZE - 1];
     }
-
-    T const *data() const noexcept {
-        return elements;
-    }
-
-    T *data() noexcept {
-        return elements;
-    }
-
-    T const *begin() const noexcept {
-        return elements;
-    }
-
-    T *begin() noexcept {
-        return elements;
-    }
-
-    T const *end() const noexcept {
-        return elements + N;
-    }
-
-    T *end() noexcept {
-        return elements + N;
-    }
+private:
+    T *elements;
+    size_t N;
 };
 
 #endif
