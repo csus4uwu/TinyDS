501e05f5beb2713aa2934af220e29f49e53994c8
Csus4 <2334085989@qq.com>
2024-10-27 18:38:08 +0800

    <update> Queue

--- Sequence/Queue.hpp
+++ Sequence/Queue.hpp
@@ -18,6 +18,48 @@ struct Queue {
         delete[] elements;
         elements = nullptr;
     }
+
+    void push(T const &_val) {
+        elements[ed++] = _val;
+    }
+    void pop() {
+        if (this->empty()) {
+            throw std::out_of_range("nothing to pop");
+        }
+        st++;
+    }
+
+    T const &back() const {
+        if (this->empty()) {
+            throw std::out_of_range("The Queue is Empty!");
+        }
+        return elements[ed - 1];
+    }
+    T &back() {
+        if (this->empty()) {
+            throw std::out_of_range("The Queue is Empty!");
+        }
+        return elements[ed - 1];
+    }
+    T const &front() {
+        if (this->empty()) {
+            throw std::out_of_range("The Queue is Empty!");
+        }
+        return elements[st];
+    }
+    T &front() {
+        if (this->empty()) {
+            throw std::out_of_range("The Queue is Empty!");
+        }
+        return elements[st];
+    }
+
+    size_t size() {
+        return ed - st;
+    }
+    bool empty() {
+        return size() == 0;
+    }
 private:
     T *elements;
     size_t st;
