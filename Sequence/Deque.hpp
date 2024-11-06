a5332beb60b4a6f5026d097d0c829c8557517ca6
Csus4 <2334085989@qq.com>
2024-11-06 20:07:19 +0800

    <new> Deque (todo)

---
+++ Sequence/Deque.hpp
@@ -0,0 +1,69 @@
+// TODO
+#ifndef Deque_H
+#define Deque_H 1
+
+#include <cstddef>   // size_t
+#include <stdexcept> // std::out_of_range
+
+constexpr size_t MAX_SIZE = 1e7;
+
+template <typename T>
+struct Deque {
+    Deque() {
+        elements = new T[MAX_SIZE];
+        st = 0;
+        ed = 0;
+    }
+    ~Deque() {
+        delete[] elements;
+        elements = nullptr;
+    }
+
+    void push(T const &_val) {
+        elements[ed++] = _val;
+    }
+    void pop() {
+        if (this->empty()) {
+            throw std::out_of_range("nothing to pop");
+        }
+        st++;
+    }
+
+    T const &back() const {
+        if (this->empty()) {
+            throw std::out_of_range("The Deque is Empty!");
+        }
+        return elements[ed - 1];
+    }
+    T &back() {
+        if (this->empty()) {
+            throw std::out_of_range("The Deque is Empty!");
+        }
+        return elements[ed - 1];
+    }
+    T const &front() {
+        if (this->empty()) {
+            throw std::out_of_range("The Deque is Empty!");
+        }
+        return elements[st];
+    }
+    T &front() {
+        if (this->empty()) {
+            throw std::out_of_range("The Deque is Empty!");
+        }
+        return elements[st];
+    }
+
+    size_t size() {
+        return ed - st;
+    }
+    bool empty() {
+        return size() == 0;
+    }
+private:
+    T *elements;
+    size_t st;
+    size_t ed;
+};
+
+#endif
