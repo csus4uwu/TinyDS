c941f0a87befcddbe53d0dd83a5b4738e6bc8076
Csus4 <2334085989@qq.com>
2024-12-06 23:23:18 +0800

    <update> BinaryTree : 实现了 insert 和 find，并添加注释。以后会更新文件规范和注释规范的文档

--- Tree/BinaryTree.hpp
+++ Tree/BinaryTree.hpp
@@ -1,41 +1,104 @@
-// TODO
+// todo
+
+/**
+ * @author  postpone (github: csus4uwu)
+ * @file  Tree/BinaryTree.hpp
+ * TinyDS 项目地址： <https://github.com/csus4uwu/TinyDS> 
+ * 欢迎 pr 或者提 issue ^v^
+ */
 #ifndef BinaryTree_H
 #define BinaryTree_H 1
 
 #include <cstddef>
 #include <map>
+#include <vector>
 
+/**
+ *  @brief  二叉搜索树的节点
+ *
+ *  @tparam  T  类型
+ */
 template <typename T>
 struct Node {
     T val;
-    Node<T> *prev;
-    Node<T> *next;
+    Node<T> *leftSon;
+    Node<T> *rightSon;
 
-    Node(const T &_val = {}) : val(_val), prev(nullptr), next(nullptr) {}
+    Node(const T &_val = {})
+        : val(_val), leftSon(nullptr), rightSon(nullptr) {}
 
     ~Node() {}
 };
 
+/**
+ *  @brief  二叉搜索树
+ *
+ *  @tparam  T  元素类型
+ */
 template <typename T>
 struct BinaryTree {
-    BinaryTree() {}
+    BinaryTree()
+        : n(0), root(nullptr) {}
     ~BinaryTree() {}
 
-    auto insert() {
-        bool success = true;
-        Node now = new Node<T>;
-        return std::make_pair(now, success);
-    }
-    T find(const T &_x) {
-        Node<T> res = new Node;
+    Node<T> *find(const T &_x) {
+        Node<T> *now = getRoot();
 
-        while (res != nullptr) {
-
+        while (now != nullptr and now->val != _x) {
+            if (now->val < _x) {
+                now = now->leftSon;
+            } else {
+                now = now->rightSon;
+            }
         }
-        return {};
+        return now;
+    }
+    Node<T> *const &find(const T &_x) const {
+        Node<T> *now = getRoot();
+
+        while (now != nullptr and now->val != _x) {
+            if (now->val < _x) {
+                now = now->leftSon;
+            } else {
+                now = now->rightSon;
+            }
+        }
+        return now;
+    }
+
+    /**
+     *  @param  _x  要找的元素
+     *  @param  _parent  传入一个引用，最后变成目标元素的父节点
+     */
+    Node<T> *find(const T &_x, Node<T> *&_parent) {
+        Node<T> *now = getRoot();
+
+        while (now != nullptr and now->val != _x) {
+            if (now->val < _x) {
+                _parent = now;
+                now = now->leftSon;
+            } else {
+                _parent = now;
+                now = now->rightSon;
+            }
+        }
+        return now;
+    }
+
+    bool insert(const T &_x) {
+        Node<T> *parent;
+        if (this->find(_x, parent) != nullptr) {
+            return false;
+        }
+        Node<T> *p = Node(_x);
+        if (_x < parent->val) {
+            parent->leftSon = p;
+        } else {
+            parent->rightSon = p;
+        }
+        return true;
     }
     void erase(const T &_x) {
-
     }
     bool contains(const T &_x) {
         return find(_x) != nullptr;
@@ -45,11 +108,15 @@ struct BinaryTree {
         return n;
     }
     bool empty() {
-        return size() == 0;
+        return root == nullptr;
     }
-private:
+    Node<T> *getRoot() {
+        return this->root;
+    }
+
+protected:
     size_t n;
-    T *root;
+    Node<T> *root;
 };
 
 #endif
